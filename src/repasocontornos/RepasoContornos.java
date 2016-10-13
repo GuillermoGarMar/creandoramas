f2155be5aa6e7cf2a444de83a23d7027db2cb54b
jbarrosgarcia <jose_bg_3@hotmail.com>
2016-10-13 09:50:48 +0200

    Proyecto normal otra vez

--- src/repasocontornos/RepasoContornos.java
+++ src/repasocontornos/RepasoContornos.java
@@ -21,39 +21,9 @@ public class RepasoContornos {
            if(x==4){
                System.out.println("Me puto aburro un huevo");
                
-               //sere capaz de aprovar contornos??
+                //sere capaz de aprovar contornos??
                
                //añado otro comentario//
-           }
-     }
-    }
-    
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package repasocontornos;
-
-/**
- *
- * @author antonio
- */
-public class RepasoContornos {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-     for(int x=0;x<=4;x++){
-           System.out.println("me estoy rascando un huevo");
-           //no tengo ni puta idea de que estoy haciendo :-))
-           if(x==4){
-               System.out.println("Me puto aburro un huevo");
-               
-               //sere capaz de aprovar contornos??
            }
            System.out.println("Vamos a volver loco esto");
      }
