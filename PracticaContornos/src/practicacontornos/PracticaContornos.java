033dd9b8f89f8672d37a702dd54e22ceeb641c7e
antonio <antonio@biruseto>
2016-10-24 09:16:22 +0200

    PracticaContornos

---
+++ PracticaContornos/src/practicacontornos/PracticaContornos.java
@@ -0,0 +1,21 @@
+
+package practicacontornos;
+
+import javax.swing.JOptionPane;
+
+
+public class PracticaContornos {
+
+   
+    public static void main(String[] args) {
+        int a=Integer.parseInt(JOptionPane.showInputDialog("numero")); //asigno valor a int a
+        int b=Integer.parseInt(JOptionPane.showInputDialog("numero")); //aisgno valor a int b
+        PracticaContornos obxSimple= new PracticaContornos();
+        
+        
+        
+       
+}
+}
+    
+
